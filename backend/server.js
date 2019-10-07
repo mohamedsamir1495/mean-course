545150b6497975c9b59e1a95463d4017aa5f0c7b
Mohamed Samir <mohamed2.samir.ext@orange.com>
2019-10-07 23:00:19 +0200

    Finished module 12

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
-const app = require("./backend/app");
-const debug = require("debug")("node-angular");
-const http = require("http");
+const app = require('./app');
+const debug = require('debug')('node-angular');
+const http = require('http');
 
 //Function that checks if the passed port is a valid port or not
 const normalizePort = val => {
@@ -21,17 +21,17 @@ const normalizePort = val => {
 
 //Function which checks which type of error happened on the node server and log the error and exit gracefully
 const onError = error => {
-  if (error.syscall !== "listen") {
+  if (error.syscall !== 'listen') {
     throw error;
   }
-  const bind = typeof port === "string" ? "pipe " + port : "port " + port;
+  const bind = typeof port === 'string' ? 'pipe ' + port : 'port ' + port;
   switch (error.code) {
-    case "EACCES":
-      console.error(bind + " requires elevated privileges");
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
       process.exit(1);
       break;
-    case "EADDRINUSE":
-      console.error(bind + " is already in use");
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
       process.exit(1);
       break;
     default:
@@ -41,14 +41,14 @@ const onError = error => {
 
 const onListening = () => {
   const addr = server.address();
-  const bind = typeof port === "string" ? "pipe " + port : "port " + port;
-  debug("Listening on " + bind);
+  const bind = typeof port === 'string' ? 'pipe ' + port : 'port ' + port;
+  debug('Listening on ' + bind);
 };
 
-const port = normalizePort(process.env.PORT || "3000");
-app.set("port", port);
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
 
 const server = http.createServer(app);
-server.on("error", onError);
-server.on("listening", onListening);
+server.on('error', onError);
+server.on('listening', onListening);
 server.listen(port);
