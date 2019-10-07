4193a4b1d3712399eb6c31d12ce65b15bee22e26
Mohamed Samir <mohamed2.samir.ext@orange.com>
2019-10-07 14:21:59 +0200

    Finished module 9 - Added Authorization mechanism

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -32,7 +32,8 @@ router.post('', checkAuth, multer({ storage: storage }).single('image'), (req, r
   const post = new Post({
     title: req.body.title,
     content: req.body.content,
-    imagePath: url + '/images/' + req.file.filename
+    imagePath: url + '/images/' + req.file.filename,
+    creator: req.userData.userId
   });
   post.save().then(createdPost => {
     res.status(201).json({
@@ -55,12 +56,19 @@ router.put('/:id', checkAuth, multer({ storage: storage }).single('image'), (req
     _id: req.body.id,
     title: req.body.title,
     content: req.body.content,
-    imagePath: imagePath
+    imagePath: imagePath,
+    creator: req.userData.userId
   });
-  Post.updateOne({ _id: req.params.id }, post).then(result => {
-    res.status(200).json({
-      message: 'Update Successful'
-    });
+  Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post).then(result => {
+    if (result.nModified > 0) {
+      res.status(200).json({
+        message: 'Update Successful'
+      });
+    } else {
+      res.status(401).json({
+        message: 'Not authorized'
+      });
+    }
   });
 });
 //
@@ -100,8 +108,14 @@ router.get('/:id', (req, res, next) => {
 });
 
 router.delete('/:id', checkAuth, (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id }).then(result => {
-    res.status(200).json({ message: 'Post deleted!' });
+  Post.deleteOne({ _id: req.params.id, creator: req.userData.userId }).then(result => {
+    if (result.n > 0) {
+      res.status(200).json({ message: 'Deletion successful' });
+    } else {
+      res.status(401).json({
+        message: 'Not authorized'
+      });
+    }
   });
 });
 
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,18 +1,18 @@
-import { Component, OnInit } from "@angular/core";
-import { FormGroup, FormControl, Validators } from "@angular/forms";
-import { PostsService } from "../posts.service";
-import { ActivatedRoute, ParamMap } from "@angular/router";
-import { Post } from "../posts.model";
-import { mimeType } from "./mime-type-validator";
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { PostsService } from '../posts.service';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Post } from '../posts.model';
+import { mimeType } from './mime-type-validator';
 @Component({
-  selector: "app-post-create",
-  templateUrl: "./post-create.component.html",
-  styleUrls: ["./post-create.component.css"],
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent implements OnInit {
-  enteredContent = "";
-  enteredTitle = "";
-  private mode = "create";
+  enteredContent = '';
+  enteredTitle = '';
+  private mode = 'create';
   private postId: string;
   post: Post;
   isLoading = false;
@@ -24,7 +24,7 @@ export class PostCreateComponent implements OnInit {
   onImagePicked(event: Event) {
     const file = (event.target as HTMLInputElement).files[0];
     this.form.patchValue({ image: file });
-    this.form.get("image").updateValueAndValidity();
+    this.form.get('image').updateValueAndValidity();
     const reader = new FileReader();
     reader.onload = () => {
       this.imagePreview = reader.result.toString();
@@ -37,7 +37,7 @@ export class PostCreateComponent implements OnInit {
     }
     this.isLoading = true;
 
-    if (this.mode === "create") {
+    if (this.mode === 'create') {
       this.postsService.addPost(this.form.value.title, this.form.value.content, this.form.value.image);
     } else {
       this.postsService.updatePost(this.postId, this.form.value.title, this.form.value.content, this.form.value.image);
@@ -48,20 +48,26 @@ export class PostCreateComponent implements OnInit {
     this.form = new FormGroup({
       title: new FormControl(null, { validators: [Validators.required, Validators.minLength(3)] }),
       content: new FormControl(null, { validators: [Validators.required] }),
-      image: new FormControl(null, { validators: [Validators.required], asyncValidators: [mimeType] }),
+      image: new FormControl(null, { validators: [Validators.required], asyncValidators: [mimeType] })
     });
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
-      if (paramMap.has("postId")) {
-        this.mode = "edit";
-        this.postId = paramMap.get("postId");
+      if (paramMap.has('postId')) {
+        this.mode = 'edit';
+        this.postId = paramMap.get('postId');
         this.isLoading = true;
         this.postsService.getPost(this.postId).subscribe(postData => {
           this.isLoading = false;
-          this.post = { id: postData._id, title: postData.title, content: postData.content, imagePath: postData.imagePath };
+          this.post = {
+            id: postData._id,
+            title: postData.title,
+            content: postData.content,
+            imagePath: postData.imagePath,
+            creator: postData.creator
+          };
           this.form.setValue({ title: this.post.title, content: this.post.content, image: this.post.imagePath });
         });
       } else {
-        this.mode = "create";
+        this.mode = 'create';
         this.postId = null;
       }
     });
